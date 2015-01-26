119d66761e2ac11ade6eccd8666a726b5d550a48
Wesley Kelly <weskelly@mac.com>
2015-01-26 17:55:51 -0500

    Split FileParser into CalibrationFileParser and CipherFileParser, didn't fix any comments

---
+++ src/SubstitutionCipherTool/CalibrationFileParser.java
@@ -0,0 +1,145 @@
+/**
+ * Author : Wesley Kelly
+ */
+package SubstitutionCipherTool;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author WesKellyPC
+ */
+public class CalibrationFileParser {
+    
+    public CalibrationFileParser() {
+        calibrationFile = null;
+        calibrationValues = new double[26];
+        
+        for (int i = 0; i < 26; i++) {
+            calibrationValues[i] = 0.0;
+        }
+        
+        calibrationAlphabet = new char[26];
+        calibrationData = "";
+    }
+    
+        //File used to calibrate (regular text)
+    private File calibrationFile;
+        //Array where the relative frequencies are stored for the cal file
+    private double[] calibrationValues;
+    private char[] calibrationAlphabet;
+        //Data that will be displayed on screen
+    private String calibrationData;
+    
+    /**
+     * Sets the calibration file (non-cipher text)
+     * @param newCalibrationFile the non-cipher file to be used as calibration
+     */
+    public void setCalibrationFile (File newCalibrationFile) {
+        calibrationFile = newCalibrationFile;
+    }
+    /**
+     * Gets the calibration file (non-cipher text)
+     * @return the non-cipher file used as calibration
+     */
+    public File getCalibrationFile () {
+        return calibrationFile;
+    }
+    /**
+     * Sets the calibration data
+     * @param calData data from the Calibration File
+     */
+    private void setCalibrationData (String calData) {
+        calibrationData = calData;
+    }
+    /**
+     * Gets the parsed data of the calibration file
+     * @return data from the Calibration file
+     */
+    public String getCalibrationData () {
+        return calibrationData;
+    }
+    
+    /**
+     * Parses, calibrates, sorts, and stores the calibration (plain-text) file. 
+     * @return whether or not the calibration was successful
+     */
+    public boolean parseAndCalibratePlaintextFile() {
+        
+        boolean finishedSuccessfully = false;
+            //if a file is selected
+        if (calibrationFile != null) {
+            try {
+                    //make a new scanner with the calibration file selected
+                Scanner line = new Scanner(calibrationFile);
+                    //count total characters for relative freq division
+                int countTotalChars = 0;
+                
+                while (line.hasNext()) {
+                    String nextLine = line.next();
+                    for (int i = 0; i < nextLine.length(); i++) {
+                        if (Character.isLetter(nextLine.charAt(i))) {
+                            countTotalChars++;
+                            calibrationValues[Character.toUpperCase(nextLine.charAt(i)) - 'A']++;
+                        }
+                    }
+                }
+                
+                String newLine = System.getProperty("line.separator");
+                    //sort the arrays
+                sortAllArraysByRelFreq();
+                
+                String calTextFieldAppend = "";
+                    
+                for (int i = 0; i < 26; i++) {
+                        //store relative frequencies rather than counts
+                    calibrationValues[i] = calibrationValues[i] / countTotalChars;
+                        //parse calibration data
+                    calTextFieldAppend += (calibrationAlphabet[i] + ": " + String.format("%.4f",calibrationValues[i]) + newLine);
+                }
+                
+                setCalibrationData(calTextFieldAppend);
+            }
+            catch (FileNotFoundException ex) {
+                System.out.println("File not found: " + ex.getMessage());
+            }
+            finishedSuccessfully = true;
+        }
+        
+        return finishedSuccessfully;
+    }
+    /**
+     * Resets both alphabets
+     */
+    private void initAlphabet() {
+        for (int i = 0; i < 26; i++) {
+            calibrationAlphabet[i] = (char)('A' + i);
+        }
+    }
+    /**
+     * Sorts all calibration data and alphabets by relative frequency
+     */
+    private void sortAllArraysByRelFreq() {
+        double tempDoub;
+        char tempChar;
+        
+        initAlphabet();
+        
+        for (int i = 0; i < 26; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                if (calibrationValues[j] > calibrationValues[i]) {
+                    
+                    tempDoub = calibrationValues[i];
+                    calibrationValues[i] = calibrationValues[j];
+                    calibrationValues[j] = tempDoub;
+                    
+                    tempChar = calibrationAlphabet[i];
+                    calibrationAlphabet[i] = calibrationAlphabet[j];
+                    calibrationAlphabet[j] = tempChar;
+                }
+            }
+        }
+    }
+}
