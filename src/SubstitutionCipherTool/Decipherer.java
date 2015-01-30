684653efb75a6221b04643b29fca9d6b1d50e264
Wesley Kelly <weskelly@mac.com>
2015-01-29 21:02:08 -0500

    Added Decipherer class

---
+++ src/SubstitutionCipherTool/Decipherer.java
@@ -0,0 +1,16 @@
+/**
+ * Title:     Decipherer Description: Uses calibrated arrays of substitution
+ * cipher data to map a calibration file to a cipher file. Displays the output.
+ * 
+ * Copyright: Copyright (c) 2015, Wesley Kelly, Cedarville University
+ * Company:   N/A
+ *
+ * @author:   Wesley Kelly, James Von Eiff
+ * @version 1.0
+ */
+
+package SubstitutionCipherTool;
+
+public class Decipherer {
+    
+}
